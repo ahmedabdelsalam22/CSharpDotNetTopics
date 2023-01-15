4129de0a3c16113c3c9079b2e1c559061c52cbe9
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-01-16 01:18:28 +0200

    some data types size

--- basic.ConsoleApp1/Program.cs
+++ basic.ConsoleApp1/Program.cs
@@ -14,7 +14,45 @@ namespace basic.ConsoleApp1
             r.Acceptdetails();
             r.Display();
 
+            //To get the exact size of a type or a variable on a particular platform
 
+            Console.WriteLine("Size of int: {0}", sizeof(int));
+
+            Console.WriteLine("Size of int: {0}", sizeof(float));
+
+            Console.WriteLine("Size of int: {0}", sizeof(double));
+
+            Console.WriteLine("Size of int: {0}", sizeof(char));
+
+
+
+
+        }
+
+
+        class Rectangle
+        {
+
+            // member variables
+            double length;
+            double width;
+
+            // evaluation length and width
+            public void Acceptdetails()
+            {
+                length = 4.5;
+                width = 3.5;
+            }
+            public double GetArea()
+            {
+                return length * width;
+            }
+            public void Display()
+            {
+                Console.WriteLine("Length: {0}", length);
+                Console.WriteLine("Width: {0}", width);
+                Console.WriteLine("Area: {0}", GetArea());
+            }
         }
 
 
