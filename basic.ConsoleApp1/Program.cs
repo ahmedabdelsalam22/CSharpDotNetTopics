df9e3f02d3e979e8e580566ee21c4a4d098fae9b
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-01-15 19:08:11 +0200

    rectangle class

--- basic.ConsoleApp1/Program.cs
+++ basic.ConsoleApp1/Program.cs
@@ -9,9 +9,44 @@ namespace basic.ConsoleApp1
         static void Main()
         {
 
+            Rectangle r = new Rectangle();
+
+            r.Acceptdetails();
+            r.Display();
 
 
         }
+
+
+        class Rectangle
+        {
+
+            // member variables
+            double length;
+            double width;
+
+            public void Acceptdetails()
+            {
+                length = 4.5;
+                width = 3.5;
+            }
+            public double GetArea()
+            {
+                return length * width;
+            }
+            public void Display()
+            {
+                Console.WriteLine("Length: {0}", length);
+                Console.WriteLine("Width: {0}", width);
+                Console.WriteLine("Area: {0}", GetArea());
+            }
+        }
+
+
+
+
+
+
     }
 
 
