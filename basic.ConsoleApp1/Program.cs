1664f613163ec782bcbe0531997adef07b336fa3
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-01-16 01:45:29 +0200

    remove comment

--- basic.ConsoleApp1/Program.cs
+++ basic.ConsoleApp1/Program.cs
@@ -9,13 +9,40 @@ namespace basic.ConsoleApp1
         static void Main()
         {
 
-            Rectangle r = new Rectangle();
+            //Rectangle r = new Rectangle();
+            //r.Acceptdetails();
+            //r.Display();
+
+            //To get the exact size of a type or a variable on a particular platform
+            //Console.WriteLine("Size of int: {0}", sizeof(int));
+            //Console.WriteLine("Size of int: {0}", sizeof(float));
+            //Console.WriteLine("Size of int: {0}", sizeof(double));
+            //Console.WriteLine("Size of int: {0}", sizeof(char));
+
+            //Object Type
+            //object obj;
+            //obj = 15;
+            //Console.WriteLine(obj);
 
 
+            // var VS dynamic 
+            // var Type
+            //var name = "Ahmed";
+            //Console.WriteLine(name);
+            // Dynamic Type
+            //dynamic num = 15; 
+            //Console.WriteLine(num);
+            //num = "Mohamed";
+            //Console.WriteLine(num);
 
 
+            // Type conversion :- type casting 
 
+            double d = 5673.74;
+            int i =  (int)d; // convert d (double) to i (int)
 
+            Console.WriteLine(d);
+            Console.WriteLine(i);
 
 
 
