f02ea8ccabc867564f54f3f2efc24404cd29764e
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-01-19 21:35:20 +0200

    Compare Strings

--- basic.ConsoleApp1/Program.cs
+++ basic.ConsoleApp1/Program.cs
@@ -148,20 +148,32 @@ namespace basic.ConsoleApp1
             //string fullname = firtsName + lastName;
             //Console.WriteLine("Full Name: {0}", fullname);
 
-            //char[] letters = { 'H', 'e', 'l', 'l', 'o' };            
+            //char[] letters = { 'H', 'e', 'l', 'l', 'o' };
             //////by using string constructor { 'H', 'e', 'l', 'l','o' };
             //string greetings = new string(letters);
             //Console.WriteLine("Greetings: {0}", greetings);
 
-            ////methods returning string { "Hello", "From", "Tutorials", "Point" };
+            //methods returning string { "Hello", "From", "Tutorials", "Point" };
             //string[] greeting = { "Hello", "From", "Tutorials", "Point" };
             //string message = String.Join(" ", greeting);
             //Console.WriteLine("Message: {0}", message);
 
             ////formatting method to convert a value
-            DateTime waiting = new DateTime(2012, 10, 10, 17, 58, 1);
-            string chat = String.Format("Message sent at {0:t} on {0:D}", waiting);
-            Console.WriteLine("Message: {0}", chat);
+            //DateTime waiting = new DateTime(2012, 10, 10, 17, 58, 1);
+            //string chat = String.Format("Message sent at {0:t} on {0:D}", waiting);
+            //Console.WriteLine("Message: {0}", chat);
+
+            string str1 = "This is test";
+            string str2 = "This is text";
+
+            if (String.Compare(str1, str2) == 0)
+            {
+                Console.WriteLine(str1 + " and " + str2 + " are equal.");
+            }
+            else
+            {
+                Console.WriteLine(str1 + " and " + str2 + " are not equal.");
+            }
 
 
         }
