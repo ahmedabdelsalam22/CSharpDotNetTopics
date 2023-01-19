f474eef9defc6c9ea32e14e49501ca4a6478e176
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-01-19 21:40:45 +0200

    Joining Strings

--- basic.ConsoleApp1/Program.cs
+++ basic.ConsoleApp1/Program.cs
@@ -184,8 +184,6 @@ namespace basic.ConsoleApp1
 
             string str = "Last night I dreamt of San Pedro";
             Console.WriteLine(str);
-            string substr = str.Substring(23);
-            Console.WriteLine(substr);
 
         }
 
