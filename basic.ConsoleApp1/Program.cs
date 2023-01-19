b973e882f950a87bb7653dd3d72ee5c3bc41ffc5
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-01-19 23:17:12 +0200

    display Box getter

--- basic.ConsoleApp1/Program.cs
+++ basic.ConsoleApp1/Program.cs
@@ -482,7 +482,8 @@ namespace basic.ConsoleApp1
             Box Box1 = new Box();   // Declare Box1 of type Box
             Box Box2 = new Box();
 
-            //double volume = 0.0;
+            double volume = 0.0;
+
             //// box 1 specification
             //Box1.height = 5.0;
             //Box1.length = 6.0;
@@ -513,6 +514,15 @@ namespace basic.ConsoleApp1
             Box2.setLength(12.0);
             Box2.setBreadth(13.0);
             Box2.setHeight(10.0);
+
+            // volume of box 1
+            volume = Box1.getVolume();
+            Console.WriteLine("Volume of Box1 : {0}", volume);
+
+            // volume of box 2
+            volume = Box2.getVolume();
+            Console.WriteLine("Volume of Box2 : {0}", volume);
+
 
         }
 
