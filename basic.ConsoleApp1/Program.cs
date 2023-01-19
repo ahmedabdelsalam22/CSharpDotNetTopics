c81f4707268add3c8f7f7cb8e8a9f701a0281868
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-01-19 22:00:51 +0200

    gave values to Books 2

--- basic.ConsoleApp1/Program.cs
+++ basic.ConsoleApp1/Program.cs
@@ -196,8 +196,21 @@ namespace basic.ConsoleApp1
             //string str = String.Join("\n", starray);
             //Console.WriteLine(str);
 
-            
-      
+            // using struct Books
+            Books Book1;   /* Declare Book1 of type Book */
+            Books Book2;   /* Declare Book2 of type Book */
+
+            /* book 1 specification */
+            Book1.title = "C Programming";
+            Book1.author = "Nuha Ali";
+            Book1.subject = "C Programming Tutorial";
+            Book1.book_id = 6495407;
+
+            /* print Book1 info */
+            Console.WriteLine("Book 1 title : {0}", Book1.title);
+            Console.WriteLine("Book 1 author : {0}", Book1.author);
+            Console.WriteLine("Book 1 subject : {0}", Book1.subject);
+            Console.WriteLine("Book 1 book_id :{0}", Book1.book_id);
 
         }
 
@@ -206,11 +219,15 @@ namespace basic.ConsoleApp1
 
         // struct 
 
-        struct Books { 
-        
+        struct Books {
 
-        
+            public string title;
+            public string author;
+            public string subject;
+            public int book_id;
+
         }
+
 
 
 
